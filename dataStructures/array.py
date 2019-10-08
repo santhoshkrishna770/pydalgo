ace7dc1fb6f84dc7acbc8d50b4ace43237f411d7
satya <aizuxconnect@gmail.com>
2019-10-08 10:05:26 +0530

    initial commit

---
+++ dataStructures/array.py
@@ -0,0 +1,33 @@
+# Implementation of arrays in python
+
+class Array:
+
+	def __init__(self,dtype=int,values):
+		pass
+
+	def append(self,element):
+		pass
+
+	def insert(index,element):
+		pass
+
+	def pop(self,index=-1):
+		pass
+
+	def remove(self,value):
+		pass
+
+	def index(self,value):
+		pass
+
+	def reverse(self):
+		pass
+
+	def __len__():
+		pass
+
+	def __repr__():
+		pass
+
+	def __str__():
+		pass
